801b7c2222d2e9e50096931a0780807d5a50449c
Morten Letnes <ort@ssb.no>
2023-04-16 18:58:10 +0200

    benchmark

--- tests/benchmark_networkanalysis.py
+++ tests/benchmark_networkanalysis.py
@@ -18,79 +18,30 @@ import sgis as sg
 
 
 def not_test_od_cost_matrix(nwa, p):
-    for search_factor in [0, 50]:
-        nwa.rules.search_factor = search_factor
-        od = nwa.od_cost_matrix(p, p)
-
-    nwa.rules.search_factor = 10
-
-    for search_tolerance in [100, 1000]:
-        nwa.rules.search_tolerance = search_tolerance
-        od = nwa.od_cost_matrix(p, p)
-
-    od = nwa.od_cost_matrix(p.loc[[349]], p)
-
-    print(
-        nwa.log[["search_tolerance", "search_factor", "percent_missing", "cost_mean"]]
-    )
+    od = nwa.od_cost_matrix(p, p)
 
 
 def not_test_get_route_frequency(nwa, p):
-    frequencies = nwa.get_route_frequencies(p, p)
-
-    # testing weight_df with and without reset_index
-    # first, weight_df will have 1 column and multiindex, second, weight_df will have
-    # three columns (origin, destination and weight. Column names doesn't matter)
-    for truefalse in [0, 1]:
-        od_pairs = pd.MultiIndex.from_product([p.index, p.index])
-        weight_df_all_10 = pd.DataFrame(index=od_pairs)
-        weight_df_all_10["weight"] = 10
-
-        if truefalse:
-            weight_df_all_10 = weight_df_all_10.reset_index()
-
-        frequencies = nwa.get_route_frequencies(p, p, weight_df=weight_df_all_10)
-
     od_pairs = pd.MultiIndex.from_product([p.index, p.index])
-    weight_df_one_pair_10 = pd.DataFrame(index=od_pairs)
-    weight_df_one_pair_10["weight"] = 1
-    weight_df_one_pair_10.loc[(349, 97), "weight"] = 100
+    weight_df_all_10 = pd.DataFrame(index=od_pairs)
+    weight_df_all_10["weight"] = 10
 
-    frequencies = nwa.get_route_frequencies(p, p, weight_df=weight_df_one_pair_10)
-
-    frequencies = nwa.get_route_frequencies(p.loc[[349]], p)
+    frequencies = nwa.get_route_frequencies(p, p, weight_df=weight_df_all_10)
 
 
 def not_test_get_route(nwa, p):
     routes = nwa.get_route(p, p)
-    routes = nwa.get_route(p, p)
-    routes = nwa.get_route(p, p)
-    routes = nwa.get_route(p, p)
-    routes = nwa.get_route(p.loc[[349]], p)
 
     # adding this for comparison purposes
     od_pairs = pd.MultiIndex.from_product([p.index, p.index])
     weight_df_all_10 = pd.DataFrame(index=od_pairs)
     weight_df_all_10["weight"] = 10
-    weight_df_all_10 = weight_df_all_10.reset_index()
-    od_pairs = pd.MultiIndex.from_product([p.index, p.index])
-    weight_df_one_pair_10 = pd.DataFrame(index=od_pairs)
-    weight_df_one_pair_10["weight"] = 1
-    weight_df_one_pair_10.loc[(349, 97), "weight"] = 100
 
 
 def not_test_service_area(nwa, p):
-    sa = nwa.service_area(p, breaks=5, dissolve=False)
+    service_areas = nwa.service_area(p, breaks=20, dissolve=True)
 
-    sa = sa.drop_duplicates(["source", "target"])
-
-    sa = nwa.service_area(p.loc[[349]], breaks=np.arange(1, 11))
-
-    sa = sa.sort_values("minutes", ascending=False)
-
-    sa = nwa.service_area(p, breaks=10, dissolve=True)
-    sa = nwa.service_area(p, breaks=15, dissolve=False)
-    sa = nwa.service_area(p, breaks=25, dissolve=True)
+    service_areas = service_areas.drop_duplicates(["source", "target"])
 
 
 def not_test_network_analysis(points_oslo, roads_oslo):
@@ -99,12 +50,14 @@ def not_test_network_analysis(points_oslo, roads_oslo):
 
     split_lines = False
 
-    buffdist = 2500
+    buffdist = 3500
 
     p = points_oslo
     p = sg.clean_clip(p, p.geometry.iloc[0].buffer(buffdist))
     p["idx"] = p.index
     p["idx2"] = p.index
+
+    print("number of points", len(p))
 
     r = roads_oslo
     r = sg.clean_clip(r, p.geometry.loc[0].buffer(buffdist * 1.1))
@@ -119,18 +72,18 @@ def not_test_network_analysis(points_oslo, roads_oslo):
     print(nwa)
 
     not_test_od_cost_matrix(nwa, p)
-    print(nwa.log.groupby("method")["minutes_elapsed"].sum())
+    print(nwa.log.groupby("method")["minutes_elapsed"].mean())
 
     not_test_get_route_frequency(nwa, p)
-    print(nwa.log.groupby("method")["minutes_elapsed"].sum())
+    print(nwa.log.groupby("method")["minutes_elapsed"].mean())
 
     not_test_get_route(nwa, p)
-    print(nwa.log.groupby("method")["minutes_elapsed"].sum())
+    print(nwa.log.groupby("method")["minutes_elapsed"].mean())
 
     print("_graph_updated_count", nwa._graph_updated_count)
 
     not_test_service_area(nwa, p)
-    print(nwa.log.groupby("method")["minutes_elapsed"].sum())
+    print(nwa.log.groupby("method")["minutes_elapsed"].mean())
 
     print("_graph_updated_count", nwa._graph_updated_count)
 
@@ -143,7 +96,8 @@ def main():
         "https://media.githubusercontent.com/media/statisticsnorway/ssb-sgis/main/tests/testdata/points_oslo.parquet"
     )
 
-    not_test_network_analysis(points_oslo, roads_oslo)
+    for _ in range(1):
+        not_test_network_analysis(points_oslo, roads_oslo)
 
 
 if __name__ == "__main__":
