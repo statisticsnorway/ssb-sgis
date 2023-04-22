bd79ec688a9ce47c0154b6afbcadc7a60ad58d74
Morten Letnes <ort@ssb.no>
2023-04-22 10:29:40 +0200

    allow naive crs. Overlay test with random polygons

--- tests/test_overlay.py
+++ tests/test_overlay.py
@@ -49,8 +49,8 @@ def test_overlay(points_oslo):
         )
         overlayed2 = sg.clean_shapely_overlay(p500, p1000, how=how)
 
-        if int(overlayed.area.sum()) != int(overlayed.area.sum()):
-            raise ValueError(int(overlayed.area.sum()) != int(overlayed.area.sum()))
+        if int(overlayed.area.sum()) != int(overlayed2.area.sum()):
+            raise ValueError(int(overlayed.area.sum()) != int(overlayed2.area.sum()))
 
         if len(overlayed) != len(overlayed2):
             raise ValueError(how, len(overlayed), len(overlayed2))
@@ -74,10 +74,44 @@ def test_overlay(points_oslo):
             )
 
 
+def test_overlay_random(n=50):
+    for _ in range(n):
+        print(_, end="\r")
+        loc_num = np.random.randint(low=50, high=150)
+        buff_num = np.random.randint(low=7, high=23)
+        for how in [
+            "intersection",
+            "difference",
+            "symmetric_difference",
+            "union",
+            "identity",
+        ]:
+            # these geometries are singlepart and fixed (in sg.buff), so
+            # results should be identical as is
+            gdf1 = sg.random_points(n, loc=loc_num).pipe(sg.buff, buff_num)
+            gdf2 = sg.random_points(n, loc=loc_num).pipe(sg.buff, buff_num * 0.5)
+
+            overlayed = gdf1.overlay(gdf2, how=how)
+            overlayed2 = sg.clean_shapely_overlay(gdf1, gdf2, how=how)
+
+            if len(overlayed) != len(overlayed2):
+                raise ValueError(how, len(overlayed), len(overlayed2))
+
+            i = 3
+            if round(sum(overlayed.area), i) != round(sum(overlayed2.area), i):
+                raise ValueError(
+                    how,
+                    i,
+                    round(sum(overlayed.area), i),
+                    round(sum(overlayed2.area), i),
+                )
+
+
 def main():
     from oslo import points_oslo
 
     test_overlay(points_oslo())
+    test_overlay_random(n=1000)
 
 
 if __name__ == "__main__":
