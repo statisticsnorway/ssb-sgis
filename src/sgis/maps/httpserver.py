6beb0087b5e6c2b1efa36ccb3586428bff3af1be
Morten Letnes <ort@ssb.no>
2023-05-10 11:15:49 +0200

    allow reuse of address

--- src/sgis/maps/httpserver.py
+++ src/sgis/maps/httpserver.py
@@ -1,38 +1,54 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
 import os
-from IPython.core.display import display, HTML
+import webbrowser
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+from IPython.core.display import HTML, display
 
 
-def run_html_server(contents: str = None, port: int = 3000):
+def run_html_server(contents: str | None = None, port: int = 3000):
     """
     Run a simple, temporary http web server for serving static HTML content.
     """
-    display_content = f"Server started at http://localhost:{port}. Click http://localhost:{port}/stop to stop server."
-    if 'JUPYTERHUB_SERVICE_PREFIX' in os.environ:
+    if "JUPYTERHUB_SERVICE_PREFIX" in os.environ:
         # Create a link using the https://github.com/jupyterhub/jupyter-server-proxy
-        display_address = os.environ['JUPYTERHUB_SERVICE_PREFIX'] + "proxy/{}/".format(port)
-        display_content = HTML(f"""
+        display_address = os.environ["JUPYTERHUB_SERVICE_PREFIX"] + "proxy/{}/".format(
+            port
+        )
+        display_content = HTML(
+            f"""
         <p>Click <a href='{display_address}'>here</a> to open in browser.</p>
         <p>Click <a href='{display_address}/stop'>here</a> to stop.</p>
-        """)
+        """
+        )
+    else:
+        display_address = f"http://localhost:{port}"
+        display_content = (
+            f"Server started at http://localhost:{port}. "
+            f"Click http://localhost:{port}/stop to stop server."
+        )
 
     class HTTPServerRequestHandler(BaseHTTPRequestHandler):
         """
-        An handler of request for the server, hosting static content.
+        A handler of request for the server, hosting static content.
         """
+
         def do_GET(self):
-            """Handle GET requests"""
+            """Handle GET requests."""
             self.send_response(200)
-            self.send_header('Content-type', 'text/html')
+            self.send_header("Content-type", "text/html")
             self.end_headers()
-            if self.path == '/stop':
-                self.wfile.write(bytes('The server is stopped', encoding='utf-8'))
+            if self.path == "/stop":
+                self.wfile.write(bytes("The server is stopped", encoding="utf-8"))
                 raise KeyboardInterrupt
             else:
-                self.wfile.write(bytes(contents, encoding='utf-8'))
+                self.wfile.write(bytes(contents, encoding="utf-8"))
 
-    with HTTPServer(('127.0.0.1', port), HTTPServerRequestHandler) as httpd:
+    HTTPServerRequestHandler.allow_reuse_address = True
+
+    with HTTPServer(("127.0.0.1", port), HTTPServerRequestHandler) as httpd:
         display(display_content)
+        webbrowser.open(display_address)
+
         try:
             httpd.serve_forever()
         except KeyboardInterrupt:
