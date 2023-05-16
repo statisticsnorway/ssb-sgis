795f23cea7ddfc029d890a86d7830c13e2001509
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2023-05-16 10:34:29 +0200

    Fix typo

--- src/sgis/maps/explore.py
+++ src/sgis/maps/explore.py
@@ -198,7 +198,7 @@ class Explore(Map):
             self._create_continous_map()
 
         if self.browser:
-            file_name = tempfile.mkdtemp(suffix='.html')
+            file_name = tempfile.mktemp(suffix='.html')
             self.map.save(file_name)
             run_html_server(file_name)
         else:
