5624af7b15be6f2938bd17147ac36e337eab6ce5
Morten Letnes <ort@ssb.no>
2024-01-22 14:16:45 +0100

    update

--- src/sgis/io/_is_dapla.py
+++ src/sgis/io/_is_dapla.py
@@ -8,5 +8,7 @@ import os
 
 def is_dapla() -> bool:
     """From https://github.com/statisticsnorway/ssb-altinn-python/blob/main/src/altinn/utils.py."""
-    jupyter_image_spec = os.environ.get("JUPYTER_IMAGE_SPEC")
-    return bool(jupyter_image_spec and "dapla-jupyterlab" in jupyter_image_spec)
+    try:
+        return os.environ["GCS_TOKEN_PROVIDER_KEY"] == "google"
+    except KeyError:
+        return False
