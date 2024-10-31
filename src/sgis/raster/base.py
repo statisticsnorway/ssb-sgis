07a2e215457020fef24399a44a415a058189cdd4
mortewle <70330502+mortewle@users.noreply.github.com>
2024-10-31 12:15:44 +0100

    remove unused

--- src/sgis/raster/base.py
+++ src/sgis/raster/base.py
@@ -48,8 +48,6 @@ def _get_shape_from_bounds(
     # miny = math.floor(miny / res) * res
     # maxy = math.ceil(maxy / res) * res
 
-    # # Compute output array shape. We guarantee it will cover the output
-    # # bounds completely
     # width = round((maxx - minx) // res)
     # height = round((maxy - miny) // res)
 
@@ -87,26 +85,6 @@ def _array_to_geojson(
 
         except Exception as err:
             raise err.__class__(array.shape, err) from err
-
-
-def _array_to_geojson_loop(array, transform, mask, processes):
-    if processes == 1:
-        return [
-            (value, shape(geom))
-            for geom, value in features.shapes(array, transform=transform, mask=mask)
-        ]
-    else:
-        with joblib.Parallel(n_jobs=processes, backend="threading") as parallel:
-            return parallel(
-                joblib.delayed(_value_geom_pair)(value, geom)
-                for geom, value in features.shapes(
-                    array, transform=transform, mask=mask
-                )
-            )
-
-
-def _value_geom_pair(value, geom):
-    return (value, shape(geom))
 
 
 def _gdf_to_arr(
@@ -178,43 +156,8 @@ def _gdf_to_arr(
     )
 
 
-def _gdf_to_geojson_with_col(gdf: GeoDataFrame, values: np.ndarray) -> list[dict]:
-    with warnings.catch_warnings():
-        warnings.filterwarnings("ignore", category=UserWarning)
-        return [
-            (feature["geometry"], val)
-            for val, feature in zip(
-                values, json.loads(gdf.to_json())["features"], strict=False
-            )
-        ]
-
-
-def _shapely_to_raster(
-    geometry: Geometry,
-    res: int | float,
-    fill: int = 0,
-    all_touched: bool = False,
-    merge_alg: Callable = MergeAlg.replace,
-    default_value: int = 1,
-    dtype: Any | None = None,
-) -> np.array:
-    shape = _get_shape_from_bounds(geometry.bounds, res=res, indexes=1)
-    transform = _get_transform_from_bounds(geometry.bounds, shape)
-
-    return features.rasterize(
-        [(geometry, default_value)],
-        out_shape=shape,
-        transform=transform,
-        fill=fill,
-        all_touched=all_touched,
-        merge_alg=merge_alg,
-        default_value=default_value,
-        dtype=dtype,
-    )
-
-
 @contextmanager
-def memfile_from_array(array: np.ndarray, **profile) -> rasterio.MemoryFile:
+    def memfile_from_array(array: np.ndarray, **profile) -> rasterio.MemoryFile:
     """Yield a memory file from a numpy array."""
     with rasterio.MemoryFile() as memfile:
         with memfile.open(**profile) as dataset:
@@ -229,32 +172,3 @@ def get_index_mapper(df: pd.DataFrame) -> tuple[dict[int, int], str]:
     idx_name = df.index.name
     return idx_mapper, idx_name
 
-
-NESSECARY_META = [
-    "path",
-    "type",
-    "bounds",
-    "crs",
-]
-
-PROFILE_ATTRS = [
-    "driver",
-    "dtype",
-    "nodata",
-    "crs",
-    "height",
-    "width",
-    "blockysize",
-    "blockxsize",
-    "tiled",
-    "compress",
-    "interleave",
-    "count",  # TODO: this should be based on band_index / array depth, so will have no effect
-    "indexes",  # TODO
-]
-
-ALLOWED_KEYS = (
-    NESSECARY_META
-    + PROFILE_ATTRS
-    + ["array", "res", "transform", "name", "date", "regex"]
-)
