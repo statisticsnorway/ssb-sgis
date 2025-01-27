da4ef364530e2ead2f0264943fce27d7629ea415
Morten Letnes <ort@ssb.no>
2025-01-27 22:21:44 +0100

    try except ImportError

--- src/sgis/io/__init__.py
+++ src/sgis/io/__init__.py
@@ -1,3 +1,6 @@
-from .dapla_functions import _read_pandas
-from .dapla_functions import read_geopandas
-from .dapla_functions import write_geopandas
+try:
+    from .dapla_functions import _read_pandas
+    from .dapla_functions import read_geopandas
+    from .dapla_functions import write_geopandas
+except ImportError:
+    pass
