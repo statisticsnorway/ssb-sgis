dfd7db0ef043d083502b2f8ed1dcbbf637356bac
Morten Letnes <ort@ssb.no>
2025-01-28 14:23:31 +0100

    Avoid ImportError

--- src/sgis/conf.py
+++ src/sgis/conf.py
@@ -1,16 +1,72 @@
 try:
     from gcsfs import GCSFileSystem
+
+    file_system = GCSFileSystem
 except ImportError:
+    import datetime
+    import glob
+    import io
+    import os
+    import pathlib
+    from concurrent.futures import ThreadPoolExecutor
+    from typing import Any
 
-    class GCSFileSystem:
-        """Placeholder."""
+    class LocalFileSystem:
+        """Mimicks GCS's FileSystem but using standard library (os, glob)."""
 
-        def __init__(self, *args, **kwargs) -> None:
-            """Placeholder."""
-            raise ImportError("gcsfs")
+        @staticmethod
+        def glob(
+            path: str,
+            detail: bool = False,
+            recursive: bool = True,
+            include_hidden: bool = True,
+            **kwargs,
+        ) -> list[dict] | list[str]:
+            """Like GCSFileSystem.glob."""
+            relevant_paths = glob.iglob(
+                path, recursive=recursive, include_hidden=include_hidden, **kwargs
+            )
 
+            if not detail:
+                return list(relevant_paths)
+            with ThreadPoolExecutor() as executor:
+                return list(executor.map(get_file_info, relevant_paths))
+
+        @classmethod
+        def ls(
+            cls, path: str, detail: bool = False, **kwargs
+        ) -> list[dict] | list[str]:
+            """Like GCSFileSystem.ls."""
+            return cls().glob(
+                str(pathlib.Path(path) / "**"), detail=detail, recursive=False, **kwargs
+            )
+
+        @staticmethod
+        def info(path) -> dict[str, Any]:
+            """Like GCSFileSystem.info."""
+            return get_file_info(path)
+
+        @staticmethod
+        def open(path: str, *args, **kwargs) -> io.TextIOWrapper:
+            """Built in open."""
+            return open(path, *args, **kwargs)
+
+        @staticmethod
+        def exists(path: str) -> bool:
+            """os.path.exists."""
+            return os.path.exists(path)
+
+    def get_file_info(path) -> dict[str, str | float]:
+        return {
+            "updated": datetime.datetime.fromtimestamp(os.path.getmtime(path)),
+            "size": os.path.getsize(path),
+            "name": path,
+            "type": "directory" if os.path.isdir(path) else "file",
+        }
+
+    file_system = LocalFileSystem
 
 config = {
     "n_jobs": 1,
-    "file_system": GCSFileSystem,
+    "file_system": file_system,
 }
