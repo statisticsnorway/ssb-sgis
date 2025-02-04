cc16b51c5a751a5026326ee8b4f08a44bea9d065
Morten Letnes <ort@ssb.no>
2025-02-04 09:15:24 +0100

    parenthesis

--- src/sgis/helpers.py
+++ src/sgis/helpers.py
@@ -28,7 +28,7 @@ def _get_file_system(
     file_system: None | AbstractFileSystem, kwargs: dict
 ) -> AbstractFileSystem:
     if (
-        file_system is not None and "filesystem" in kwargs or "file_system" in kwargs
+        file_system is not None and ("filesystem" in kwargs or "file_system" in kwargs)
     ) or ("filesystem" in kwargs and "file_system" in kwargs):
         raise ValueError("Cannot pass both filesystem and file_system.")
     file_system2 = kwargs.pop("file_system", None)
