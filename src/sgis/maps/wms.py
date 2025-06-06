a646f8e91ea5c92d15f5a19b859b3186254971d0
Morten Letnes <ort@ssb.no>
2025-06-06 13:15:53 +0200

    flere år som default

--- src/sgis/maps/wms.py
+++ src/sgis/maps/wms.py
@@ -20,7 +20,7 @@ JSON_YEARS = [str(year) for year in range(1999, 2025)]
 DEFAULT_YEARS: tuple[str] = tuple(
     str(year)
     for year in range(
-        int(datetime.datetime.now().year) - 8,
+        int(datetime.datetime.now().year) - 10,
         int(datetime.datetime.now().year) + 1,
     )
 )
