b88250fa10aae76a1011b3c25a74d0d2b5f41f67
Morten Letnes <ort@ssb.no>
2026-01-19 13:48:43 +0100

    to wkt before groupby because MUCH faster

--- src/sgis/geopandas_tools/general.py
+++ src/sgis/geopandas_tools/general.py
@@ -870,9 +870,9 @@ def make_edge_wkt_cols(gdf: GeoDataFrame) -> GeoDataFrame:
     except ValueError:
         gdf, endpoints = _prepare_make_edge_cols(gdf)
 
-    endpoints = endpoints.force_2d()
-    gdf["source_wkt"] = endpoints.groupby(level=0).first().to_wkt()
-    gdf["target_wkt"] = endpoints.groupby(level=0).last().to_wkt()
+    endpoints = endpoints.force_2d().to_wkt()
+    gdf["source_wkt"] = endpoints.groupby(level=0).first()
+    gdf["target_wkt"] = endpoints.groupby(level=0).last()
 
     if index_mapper is not None:
         gdf.index = gdf.index.map(index_mapper)
