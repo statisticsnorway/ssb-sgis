6c549e1984d8a6e5d4dc80b97a3e7a7a5834c738
Morten Letnes <ort@ssb.no>
2026-06-24 12:06:02 +0200

    cruft update

--- noxfile.py
+++ noxfile.py
@@ -23,7 +23,7 @@ except ImportError:
     raise SystemExit(dedent(message)) from None
 
 package = "sgis"
-python_versions = ["3.11", "3.12"]
+python_versions = ["3.12", "3.13"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
